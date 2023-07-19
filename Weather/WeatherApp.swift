3f087a40c4641302c84db8ea1329f8d3dadf5ff3
elkraps <dobroisreal@mail.ru>
2023-07-19 16:52:54 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Vlad Dzirko on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
